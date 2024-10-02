ae6157a748ce42820ae7017d21f41d90ac3ae58b
Roman Elizarov <elizarov@gmail.com>
2024-10-02 17:48:53 +0300

    Better derivation explanation

--- src/Fact.kt
+++ src/Fact.kt
@@ -34,7 +34,9 @@ fun Fact.explainDerivation(): List<String> {
     }
     var mostComplex: Formula = formula
     var depth = 0
+    val usage = HashMap<Fact, Int>()
     fun Fact.explainDerivationImpl() {
+        usage[this] = (usage[this] ?: 0) + 1
         if (!explained.add(this)) return
         if (formula.complexity > mostComplex.complexity) mostComplex = formula
         when (this) {
@@ -45,23 +47,32 @@ fun Fact.explainDerivation(): List<String> {
                 val shiftedImpl = implication.formula.normalize(premise.formula.normalVariablesSize) as Implication
                 val map = unify(premise.formula, shiftedImpl.a)!!
                 val id = makeId(nextId++)
-                val s = "${premise.withId()}, ${implication.withId()}"
+                val s0 = "[$id] $formula"
+                val s1 = premise.withId().toString()
+                val s2 = implication.withId().toString()
                 depth++
-                result += "MP: $s"
-                result += "    ${"-".repeat(s.length)}"
-                result += "        [$id] $formula"
-                result += "         |  1. rename variables used in implication ${implication.withId()}"
-                result += "         |       implication LHS: ${shiftedImpl.a}"
-                result += "         |       implication RHS: ${shiftedImpl.b}"
-                result += "         |  2. unify implication LHS with premise ${premise.withId()}"
-                result += "         |      substitution map: ${map.toList().joinToString(", ") { "${it.first} := ${it.second}" }}"
-                result += "         |  3. derive conclusion: ${shiftedImpl.b.substitute(map)}"
+                result += "-".repeat(maxOf(s0.length, maxOf(s1.length, s2.length) + 4))
+                result += s0
+                result += "MP: $s1"
+                result += "    $s2"
+                result += "    |  1. rename variables used in implication ${implication.withId()}"
+                result += "    |       implication LHS: ${shiftedImpl.a}"
+                result += "    |       implication RHS: ${shiftedImpl.b}"
+                result += "    |  2. unify implication LHS with premise ${premise.withId()}"
+                result += "    |      substitution map: ${map.toList().joinToString(", ") { "${it.first} := ${it.second}" }}"
+                result += "    |  3. derive conclusion: ${shiftedImpl.b.substitute(map)}"
                 ids[formula] = id
             }
         }
     }
     explainDerivationImpl()
-    result += "# Derivation depth is $depth"
+    result += "# Actual derivation depth is $depth (computed depth is ${this.depth})"
     result += "# Derivation complexity is ${mostComplex.complexity} with $mostComplex"
+    val topN = 3
+    val popularity = usage.toList().sortedByDescending { it.second }.take(topN)
+    result += "# Top $topN most popular facts are:"
+    for ((fact, pop) in popularity) {
+        result += "#   $pop: ${fact.withId()}"
+    }
     return result
 }
